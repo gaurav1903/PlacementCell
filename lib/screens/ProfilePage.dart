ad8b69e0f2a3c9af0cfb4dad8fe2a67556d8d7fc
gaurav arora <gaurav1903arora@gmail.com>
2021-12-19 22:16:54 +0530

    fixed 1st dropdown butto

--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
 import 'dart:io';
@@ -6,7 +9,6 @@ import 'package:image_picker/image_picker.dart';
 class ProfilePage extends StatefulWidget {
   String url;
   ProfilePage(this.url);
-
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
@@ -14,51 +16,112 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   var PickedImage;
   final k = GlobalKey<FormState>();
+  List skills = [];
+  List domain = [];
+  var skillval, domainval;
+  Future<void> getting_data() async {
+    await FirebaseFirestore.instance
+        .collection('skills')
+        .doc('l1G4TFS9carHqG25KrfJ')
+        .get()
+        .then((value) {
+      skills = value.data()?['skills'].toList();
+      skillval = skills[0];
+    });
+    await FirebaseFirestore.instance
+        .collection('domain')
+        .doc('h16ILMhCBNG96ItFqhzT')
+        .get()
+        .then((value) {
+      domain = value.data()?['domain'].toList();
+      domainval = domain[0];
+    });
+    return;
+  }
+
+  Future datafetching = Future.value();
+  @override
+  void initState() {
+    datafetching = getting_data();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.blue.shade50,
-        child: Column(
-          children: [
-            Container(
-              height: 250,
-              width: MediaQuery.of(context).size.width,
-              child: GestureDetector(
-                child: PickedImage == null
-                    ? Image.asset('assets/selectImage.jpg',
-                        fit: BoxFit.fitWidth)
-                    : Image.file(File((PickedImage as XFile).path),
-                        width: MediaQuery.of(context).size.width,
-                        fit: BoxFit.fitWidth),
-                onTap: () {
-                  ImagePicker.platform
-                      .getImage(source: ImageSource.gallery)
-                      .then((value) {
-                    setState(() {
-                      PickedImage = value;
-                    });
-                  });
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            Form(
-              key: k,
-              child: Column(children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Field("Batch year", TextInputType.number),
-                    SizedBox(width: 50),
-                    Field(
-                        "CGPA", TextInputType.numberWithOptions(decimal: true))
-                  ],
-                )
-              ]),
-            ),
-          ],
-        ));
+    return FutureBuilder(
+        future: datafetching,
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting)
+            return Center(child: CircularProgressIndicator());
+          return Container(
+              color: Colors.blue.shade50,
+              child: Column(
+                children: [
+                  Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    child: GestureDetector(
+                      child: PickedImage == null
+                          ? Image.asset('assets/selectImage.jpg',
+                              fit: BoxFit.fitWidth)
+                          : Image.file(File((PickedImage as XFile).path),
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.fitWidth),
+                      onTap: () {
+                        ImagePicker.platform
+                            .getImage(source: ImageSource.gallery)
+                            .then((value) {
+                          setState(() {
+                            PickedImage = value;
+                          });
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Form(
+                    key: k,
+                    child: Column(children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Field("Batch year", TextInputType.number),
+                          SizedBox(width: 50),
+                          Field("CGPA",
+                              TextInputType.numberWithOptions(decimal: true))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          DropdownButton(
+                            value: skillval,
+                            onChanged: (val) {
+                              setState(() {
+                                skillval = val;
+                              });
+                            },
+                            items: skills.map((e) {
+                              return DropdownMenuItem(
+                                child: Text(e.toString()),
+                                value: e.toString(),
+                              );
+                            }).toList(),
+                          ),
+                          DropdownButton(
+                            items: [],
+                            onChanged: null,
+                          )
+                        ],
+                      )
+                    ]),
+                  ),
+                ],
+              ));
+        });
   }
 }
 
